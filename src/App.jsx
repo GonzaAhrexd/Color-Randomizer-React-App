59facad10c0a1dd95554efb34507c95baabeaf7a
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2022-12-28 23:38:24 -0300

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,61 +3,96 @@ import { useState } from 'react'
 import RandomColors from './components/RandomColors'
 import CantidadTarjetas from './components/CantidadTarjetas'
 import CambiarModo from './components/CambiarModo'
+import { ToastContainer, toast } from 'react-toastify'
+import convert from 'hsl-to-hex'
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
 
-  const [cantidad, setCantidad] = useState(16)
+  const [cantidad, setCantidad] = useState(1)
 
-  const [redo,setRedo] = useState(0)
+  const [redo, setRedo] = useState(0)
 
 
- 
-    const less = ()=>{
-      if(cantidad>1)
-      setCantidad(cantidad-1)
-    }
-  const more = ()=>{
-    if(cantidad<16)
-    setCantidad(cantidad+1)
-    
+
+  const less = () => {
+    if (cantidad > 1)
+      setCantidad(cantidad - 1)
+  }
+  const more = () => {
+    if (cantidad < 16)
+      setCantidad(cantidad + 1)
+
   }
 
-  function mostrarTarjetas(){
+
+
+  function mostrarTarjetas() {
     let tarjetas = []
-  for (let index = 0; index < cantidad; index++) {
-    tarjetas.push( <RandomColors></RandomColors>)
+    for (let index = 0; index < cantidad; index++) {
+      tarjetas.push(<RandomColors notify={notify}></RandomColors>)
+    }
+    return tarjetas;
   }
-      return tarjetas;
+
+  const notify = () => {
+    toast.success(' Copied to clipboard!', {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
   }
+
 
   return (
     <div className="App">
-<header> 
-      <h1>Color randomizer</h1>
-<section className="botonesResponsive"> 
-     <section className='containerAmount'>
-    <CantidadTarjetas cantidad={cantidad} more={more} less={less}></CantidadTarjetas>
-     </section>
-    <section class="seccionCambiar"> 
-     <CambiarModo redo = {redo} setRedo={setRedo}></CambiarModo>
-     </section>
-</section>
-  </header>
+      <header>
+        <h1>Color randomizer</h1>
+        <ToastContainer
+          position="top-center"
+          autoClose={1000}
+          limit={1}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
 
-     <main className='container'>
+        />
 
-     {mostrarTarjetas()}
-   
-     </main>
+        <section className="botonesResponsive">
+          <section className='containerAmount'>
+            <CantidadTarjetas cantidad={cantidad} more={more} less={less}></CantidadTarjetas>
+          </section>
+          <section class="seccionCambiar">
+            <CambiarModo redo={redo} setRedo={setRedo}></CambiarModo>
+          </section>
+        </section>
+      </header>
 
-     <footer className="botones">
-      
-      <section className='containerAmount'>
-    <CantidadTarjetas cantidad={cantidad} more={more} less={less}></CantidadTarjetas>
-     </section>
-    <section class="seccionCambiar"> 
-     <CambiarModo redo = {redo} setRedo={setRedo}></CambiarModo>
-     </section>
-     </footer>
+      <main className='container'>
+
+        {mostrarTarjetas()}
+
+      </main>
+
+      <footer className="botones">
+
+        <section className='containerAmount'>
+          <CantidadTarjetas cantidad={cantidad} more={more} less={less}></CantidadTarjetas>
+        </section>
+        <section class="seccionCambiar">
+          <CambiarModo redo={redo} setRedo={setRedo}></CambiarModo>
+        </section>
+      </footer>
 
     </div>
 
--- src/components/RandomColors.jsx
+++ src/components/RandomColors.jsx
@@ -2,68 +2,78 @@ import React from 'react'
 import styled from 'styled-components'
 import convert from 'hsl-to-hex'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { Toaster, toast } from 'react-hot-toast'
-function RandomColors() {
+
+
+function random(max){
+  return Math.floor(Math.random()*max)
+}
+
+
+
+function RandomColors(props) {
   
 
-    function random(max){
-        return Math.floor(Math.random()*max)
-    }
-    
-   let values = [random(255),random(100),random(100)]
-   let hsl = `hsl(${values[0]},${values[1]}%,${values[2]}%)`
    
-   let hex = convert(values[0],values[1],values[2])
+let values = [random(255),random(100),random(100)]
+let hsl = `hsl(${values[0]},${values[1]}%,${values[2]}%)`
 
-  const Cards = styled.div`
-  border-color: white;
-  border-style: solid;
-  width: 10vw;
-  height: 10vw;
-  border-radius: 20px;
-  margin-bottom: 20px;
-  background-color: ${hsl};
-  color: transparent;
+let hex = convert(values[0],values[1],values[2])
+
+const Cards = styled.div`
+border-color: white;
+border-style: solid;
+width: 10vw;
+height: 10vw;
+border-radius: 20px;
+margin-bottom: 20px;
+background-color: ${hsl};
+color: transparent;
+display: flex;
+justify-content: center;
+align-items: center;
+align-content: center;
+&:hover{
+  cursor: pointer;
+  width: 11vw;
+  height: 11vw;
+  color: black;
   display: flex;
   justify-content: center;
   align-items: center;
   align-content: center;
-  &:hover{
-    cursor: pointer;
-    width: 11vw;
-    height: 11vw;
-    color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    weight: 700;
-    text-shadow: 2px 0 #fff, -2px 0 #fff, 0 2px #fff, 0 -2px #fff,
-               1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff;
-               @media (max-width: 638px) {
-                width: 95vw;
-                height: 26vw;
-                border-color: transparent;
-                border-radius: 5px;
-                margin-bottom: 2vw;
-                margin-right: 3vw;
-            }
+  weight: 700;
+  text-shadow: 2px 0 #fff, -2px 0 #fff, 0 2px #fff, 0 -2px #fff,
+             1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff;
+             @media (max-width: 638px) {
+              width: 95vw;
+              height: 26vw;
+              border-color: transparent;
+              border-radius: 5px;
+              margin-bottom: 2vw;
+              margin-right: 3vw;
+          }
 
-              }
-              @media (max-width: 638px) {
-                width: 95vw;
-                height: 25vw;
-                border-color: transparent;
-                border-radius: 5px;
-                margin-bottom: 2vw;
-                margin-right: 3vw;
             }
-  `
+            @media (max-width: 638px) {
+              width: 95vw;
+              height: 25vw;
+              border-color: transparent;
+              border-radius: 5px;
+              margin-bottom: 2vw;
+              margin-right: 3vw;
+          }
+`
+
+
     return (
+      
 <>
+
+
+
     <div className='ColorCards'> 
     <CopyToClipboard text={hex}>
-    <Cards  onClick={() => toast('✅ Copied to clipboard')}>
+    <Cards  onClick={() => {props.notify()}}>
 
      <b>  Click to copy HEX </b>
 
@@ -72,7 +82,7 @@ function RandomColors() {
     
    </CopyToClipboard>
 
-      <Toaster/>
+    
     
 
     <div className='information'> 
